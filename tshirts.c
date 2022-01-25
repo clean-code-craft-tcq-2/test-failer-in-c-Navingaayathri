39f15c7af194d8566679e508193bf21b17c13c16
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-01-25 10:48:32 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -14,10 +14,10 @@ char size(int cms) {
 }
 
 int main() {
-    char sizeName;
-    assert(sizeName != '\0');
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
