90f06977a97aa4b4d45c3c2b8ab5d31e940c0a47
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-01-25 11:36:00 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
+int numOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
+
+typedef struct {
+    int pairNumber;
+    const char* majColor;
+    const char* minColor;
+} ColorPair;
+
+void printColorMap(int pairNumber, const char *majorColor, const char *minorColor ) {
+	printf("%d | %s | %s\n", pairNumber, majorColor, minorColor);
+}
+
+ColorPair getColorInfoFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    colorPair.pairNumber = pairNumber;
+    colorPair.majColor = majorColor[(pairNumber / numOfMajorColors)];
+    colorPair.minColor = minorColor[(pairNumber % numOfMinorColors)];
+    return colorPair;
+}
+
+void testPairNumberToColor(int pairNumber, const char* expectedMajorColor, const char* expectedMinorColor){
+    ColorPair colorPair = getColorInfoFromPairNumber(pairNumber);
+    assert(colorPair.pairNumber == pairNumber);
+    assert(colorPair.majColor == expectedMajorColor);
+    assert(colorPair.minColor == expectedMinorColor);
+}
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printColorMap(i * 5 + j, majorColor[i], minorColor[i]);
+            testPairNumberToColor(i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
