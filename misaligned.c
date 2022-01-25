ad3b55140250e7e41d9f45bea890122d246abd96
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-01-25 18:57:07 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-int numOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
-int numOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
 
-typedef struct {
-    int pairNumber;
-    const char* majColor;
-    const char* minColor;
-} ColorPair;
+struct Colorpair
+{
+	int Num;
+	char MajorColor[8];
+	char MinorColor[8];
+};
 
-void printColorMap(int pairNumber, const char *majorColor, const char *minorColor ) {
-	printf("%d | %s | %s\n", pairNumber, majorColor, minorColor);
-}
+Colorpair colorpair[25];
 
-ColorPair getColorInfoFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    colorPair.pairNumber = pairNumber;
-    colorPair.majColor = majorColor[(pairNumber / numOfMajorColors)];
-    colorPair.minColor = minorColor[(pairNumber % numOfMinorColors)];
-    return colorPair;
-}
-
-void testPairNumberToColor(int pairNumber, const char* expectedMajorColor, const char* expectedMinorColor){
-    ColorPair colorPair = getColorInfoFromPairNumber(pairNumber);
-    assert(colorPair.pairNumber == pairNumber);
-    assert(colorPair.majColor == expectedMajorColor);
-    assert(colorPair.minColor == expectedMinorColor);
+void PrintOnConsole( int i, int j)
+{
+printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
 }
 
 int printColorMap() {
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printColorMap(i * 5 + j, majorColor[i], minorColor[i]);
-            testPairNumberToColor(i * 5 + j, majorColor[i], minorColor[i]);
+    
+    int i = 0, j = 0,ColorPairIdx=0;
+    for(i = 0; i < 5; i++) 
+	{
+        for(j = 0; j < 5; j++)
+		{
+			
+			colorpair[ColorPairIdx].Number = i * 5 + j;
+			strcpy(colorpair[ColorPairIdx].MajorColor , majorColor[i]);
+			strcpy(colorpair[ColorPairIdx].MinorColor , minorColor[i]);
+			PrintOnConsole(i,j);	
+		        ColorPairIdx++;
+				          
         }
     }
     return i * j;
@@ -46,6 +42,9 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+	assert(colorpair[15].Num == 15);	
+	assert(strcmp(colorpair[15].MajorColor, "Black") == 0);
+	assert(strcmp(colorpair[15].MinorColor, "Slate") == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
