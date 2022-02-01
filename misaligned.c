06b9f91d59713678e78ce3313ed05bbd4db0d629
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-02-01 21:48:45 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -22,7 +22,7 @@ printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
 
 int printColorMap() {
     
-    int i = 0, j = 0,ColorPairIdx=0;
+    int i = 0, j = 0, ColorPairIdx=0;
     for(i = 0; i < 5; i++) 
 	{
         for(j = 0; j < 5; j++)
@@ -30,10 +30,9 @@ int printColorMap() {
 			
 			colorpair[ColorPairIdx].Num = i * 5 + j;
 			strcpy(colorpair[ColorPairIdx].MajorColor , majorColor[i]);
-			strcpy(colorpair[ColorPairIdx].MinorColor , minorColor[i]);
+			strcpy(colorpair[ColorPairIdx].MinorColor , minorColor[j]);
 			PrintOnConsole(i,j);	
-		        ColorPairIdx++;
-				          
+		        ColorPairIdx++;				          
         }
     }
     return i * j;
@@ -42,9 +41,12 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-	assert(colorpair[15].Num == 15);	
+	assert(colorpair[23].Number == 23);	
+	assert(strcmp(colorpair[23].MajorColor, "Violet") == 0);
+	assert(strcmp(colorpair[23].MinorColor, "Green") == 0);
+	assert(colorpair[15].Number == 15);	
 	assert(strcmp(colorpair[15].MajorColor, "Black") == 0);
 	assert(strcmp(colorpair[15].MinorColor, "Slate") == 0);
-    printf("All is well (maybe!)\n");
+    printf("All is well (Yes,it is! :> )\n");
     return 0;
 }
